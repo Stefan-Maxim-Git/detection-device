89cedbce58957fdf7ad31d6b563e56ef7a3aa880
Stefan Maxim <stemaxx2002@gmail.com>
2025-06-20 22:14:00 +0100

    Fixed error where requests always sended HTTPS requests instead of HTTP to the Ollama server. Changed default prompt for better results from the SLM.

--- src/info_server.py
+++ src/info_server.py
@@ -1,10 +1,14 @@
-import socket
+import socket, requests, time
 
-class LabelHandlerServer():
-    def __init__(self, label_port=5001, resume_port=5002, host='localhost'):
+class LabelProcessingServer():
+    def __init__(self, label_port=5001, resume_port=5002, host='localhost', model="gemma:2b-instruct"):
         self.label_port = label_port
         self.resume_port = resume_port
         self.host = host
+        self.model = model
+
+        self.session = requests.Session()
+        self.session.trust_env = False
         # Add a print for checking if ports were assigned (maybe)
 
     def listen_for_label(self):
@@ -23,8 +27,12 @@ class LabelHandlerServer():
                 self.resume_detection()
 
     def process_label(self, label):
-        # Do work here later...
+        fun_fact = self.query_ollama(label)
+        print(f"Fun fact about {label}: {fun_fact}")
+        # Add TTS later
+        time.sleep(4)
         print(f"Processed label {label}. Resuming pipeline...")
+
 
     def resume_detection(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as resume_sock:
@@ -33,7 +41,27 @@ class LabelHandlerServer():
                 resume_sock.sendall(b"resume")
             except Exception as e:
                 print("Failed to send resume signal - SLM/TTS")
+    
+    def query_ollama(self, label):
+        prompt = (
+            f"Tell me a random fact about {label}s in two short sentences. "
+            "Use simple language. Do not use scientific terms."
+        ) 
+        ollama_local_url = "http://127.0.0.1:11434/api/generate"
+        data = {
+            "model": self.model,
+            "prompt": prompt,
+            "stream": False
+        }
 
+        try:
+            response = self.session.post(ollama_local_url, json=data, timeout=15)
+            response.raise_for_status()
+            return response.json()['response']
+        except Exception as e:
+            print(f"Ollama API error: {e}")
+            return "No response"
+        
 if __name__ == "__main__":
-    server = LabelHandlerServer()
+    server = LabelProcessingServer()
     server.listen_for_label()
